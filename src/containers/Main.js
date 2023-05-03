a58093b2d469dfbaca5f3fdb0ddc949c9d6e1438
Joseph Abel <joseph.abel@greelow.com>
2023-05-02 23:20:50 -0300

    fix

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -3,16 +3,11 @@ import Header from "../components/header/Header";
 import Greeting from "./greeting/Greeting";
 import StackProgress from "./skillProgress/skillProgress";
 import WorkExperience from "./workExperience/WorkExperience";
-import Projects from "./projects/Projects";
 import StartupProject from "./StartupProjects/StartupProject";
 import Achievement from "./achievement/Achievement";
-import Blogs from "./blogs/Blogs";
 import Footer from "../components/footer/Footer";
-import Talks from "./talks/Talks";
-import Podcast from "./podcast/Podcast";
 import Education from "./education/Education";
 import ScrollToTopButton from "./topbutton/Top";
-import Twitter from "./twitter-embed/twitter";
 import Profile from "./profile/Profile";
 import SplashScreen from "./splashScreen/SplashScreen";
 import {splashScreen} from "../portfolio";
@@ -53,16 +48,9 @@ const Main = () => {
             <Header />
             <Greeting />
             <Habilidades />
-            <StackProgress />
             <Education />
             <WorkExperience />
-            <Projects />
-            <StartupProject />
             <Achievement />
-            <Blogs />
-            <Talks />
-            <Twitter />
-            <Podcast />
             <Profile />
             <Footer />
             <ScrollToTopButton />
--- src/containers/achievement/Achievement.js
+++ src/containers/achievement/Achievement.js
@@ -10,48 +10,44 @@ export default function Achievement() {
     return null;
   }
   return (
-    <Fade bottom duration={1000} distance="20px">
-      <div className="main" id="achievements">
-        <div className="achievement-main-div">
-          <div className="achievement-header">
-            <h1
-              className={
-                isDark
-                  ? "dark-mode heading achievement-heading"
-                  : "heading achievement-heading"
-              }
-            >
-              {achievementSection.title}
-            </h1>
-            <p
-              className={
-                isDark
-                  ? "dark-mode subTitle achievement-subtitle"
-                  : "subTitle achievement-subtitle"
-              }
-            >
-              {achievementSection.subtitle}
-            </p>
-          </div>
-          <div className="achievement-cards-div">
-            {achievementSection.achievementsCards.map((card, i) => {
-              return (
-                <AchievementCard
-                  key={i}
-                  isDark={isDark}
-                  cardInfo={{
-                    title: card.title,
-                    description: card.subtitle,
-                    image: card.image,
-                    imageAlt: card.imageAlt,
-                    footer: card.footerLink
-                  }}
-                />
-              );
-            })}
-          </div>
-        </div>
+    <div className="main" id="achievements">
+      <div className="achievement-header">
+        <h1
+          className={
+            isDark
+              ? "dark-mode heading achievement-heading"
+              : "heading achievement-heading"
+          }
+        >
+          {achievementSection.title}
+        </h1>
+        <p
+          className={
+            isDark
+              ? "dark-mode subTitle achievement-subtitle"
+              : "subTitle achievement-subtitle"
+          }
+        >
+          {achievementSection.subtitle}
+        </p>
       </div>
-    </Fade>
+      <div className="achievement-cards-div">
+        {achievementSection.achievementsCards.map((card, i) => {
+          return (
+            <AchievementCard
+              key={i}
+              isDark={isDark}
+              cardInfo={{
+                title: card.title,
+                description: card.subtitle,
+                image: card.image,
+                imageAlt: card.imageAlt,
+                footer: card.footerLink
+              }}
+            />
+          );
+        })}
+      </div>
+    </div>
   );
 }
